a9d1056b950abada7d338ead9b8d59b5be88873a
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2018-05-22 21:13:04 +0800

    bugfix

--- src/longest_palindromic_substring.c
+++ src/longest_palindromic_substring.c
@@ -14,10 +14,8 @@
 #include <linux_config.h>
 #include <misc_utils.h>
 
-static int max = 0;
-
 static char *
-scan_eve_palindrome(char *s)
+scan_eve_palindrome(char *s, int *maxp)
 {
     char * l=s;
     char * r=s+1;
@@ -33,9 +31,9 @@ scan_eve_palindrome(char *s)
              } else {
                  break;
              }
-        } while(lt=='\0' || lr=='\0');
-        if (length > max) {
-            res=lt+1, max=length; 
+        } while(*lt!='\0' && *lr!='\0');
+        if (length > *maxp) {
+            res=lt+1, *maxp=length; 
         }
         length=0, l++, r++;
         if (*r == '\0') break;
@@ -44,7 +42,7 @@ scan_eve_palindrome(char *s)
 }
 
 static char *
-scan_odd_palindrome(char *s)
+scan_odd_palindrome(char *s, int *maxp)
 {
     char * m = s;
     char * lt;
@@ -60,9 +58,9 @@ scan_odd_palindrome(char *s)
              } else {
                  break;
              }
-        } while(lt=='\0' || lr=='\0');
-        if (length > max) {
-            res=lt+1, max=length; 
+        } while(*lt!='\0' && *lr!='\0');
+        if (length > *maxp) {
+            res=lt+1, *maxp=length; 
         }
         length=-1, m++;
         if (*m == '\0') break;
@@ -77,21 +75,28 @@ longestPalindrome(char* s)
     char *eve = NULL;
     char *odd = NULL;
     int eve_len=0, odd_len=0;
+    char *result = NULL;
+    int len = 0;
+    int max = 0;
 
     if (strlen(s) <= 1) return s;
 
     memset(ss, 0, strlen(s)+2);
     memcpy(ss+1, s, strlen(s));
     
-    eve = scan_eve_palindrome(ss+1), eve_len = max;
-    odd = scan_odd_palindrome(ss+1), odd_len = max;
+    eve = scan_eve_palindrome(ss+1, &max), eve_len = max;
+    max = 0;
+    odd = scan_odd_palindrome(ss+1, &max), odd_len = max;
 
-    return eve_len > odd_len ? eve : odd; 
+    result = eve_len > odd_len ? eve : odd; 
+    len = eve_len > odd_len ? eve_len : odd_len; 
+    result[len] = '\0';
+    return result;
 }
 
 int main(int argc, char **argv) 
 {
-    char *s = "abcddsdfadsds";
+    char *s = "cbbd";
     printf("result is %s\n", longestPalindrome(s));
     return 0;
 }
