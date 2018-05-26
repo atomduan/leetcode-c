402019b1e47c423d90e71740bc6bfe9edf8a6f03
atomduan <atomduan@gmail.com>
2018-05-26 14:20:42 +0800

    add new

--- src/reverse_integer.c
+++ src/reverse_integer.c
@@ -18,13 +18,31 @@
  *32-bit signed integer range: [−231,  231 − 1]. 
  *For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
  */
+#include <linux_config.h>
+#include <misc_utils.h>
 
 static int reverse(int x) {
-    return 0;
+    int truck = 0;
+    int xbase = x;
+    int result = 0;
+    int limit = ~(1<<31);
+    if (x > (int)limit || x < 1-(int)limit) return 0;
+    limit = limit/10;
+    while(1) {
+        truck = xbase % 10;
+        if ((result>0&&result>limit) || (result<0&&result<-1*limit)) {
+            return 0;
+        } else {
+            result = result*10 + truck;
+        }
+        xbase = xbase / 10;
+        if (xbase == 0) break;
+    }
+    return result;
 }
 
 int main(int argc, char **argv)
 {
-    reverse(10);
+    printf("reverse is %d\n",reverse(1534236469));
     return 0;
 }
