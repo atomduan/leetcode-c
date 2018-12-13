78b6442841310ef0d5dce99ea19d7fa6697db2bb
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2018-12-13 20:14:15 +0800

    add

--- src/4sum.c
+++ src/4sum.c
@@ -13,11 +13,47 @@
  *   [-2,  0, 0, 2]
  * ]
  */
+#include <linux_config.h>
 
-/**
- * Return an array of arrays of size *returnSize.
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int** fourSum(int* nums, int numsSize, int target, int* returnSize) {
-    
+void *
+leet_malloc(size_t memn)
+{
+    void *res = NULL;
+    if (memn > 0) {
+        res = malloc(memn);
+        memset(res,0,memn);
+    }
+    return res;
+}
+
+int** 
+fourSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    int **res = NULL;
+    return res; 
+}
+
+int main(int argc, char **argv) 
+{
+    int target = 0;
+    int returnSize = 0;
+    int nums[] = {-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0};
+    int numsSize = sizeof(nums)/sizeof(int);
+    int ** res = fourSum(nums, numsSize, target, &returnSize);
+    int vi=0,vj=0,vk=0,vl=0,i=0,*pi=NULL;
+    printf("returnSize is %d\n", returnSize);
+    if (res != NULL) {
+        for (i=0; i<returnSize; i++) {
+            pi = res[i];
+            vi = pi[0];
+            vj = pi[1];
+            vk = pi[2];
+            vl = pi[3];
+            printf("result tuple is %d,%d,%d,%d\n",vi,vj,vk,vl);
+        }
+        free(res);
+    } else {
+        printf("return res is NULL\n");
+    }
+    return 0;
 }
