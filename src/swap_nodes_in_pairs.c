934c57fea0707f28600fc1f3609d447d9b388a2d
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2018-12-24 10:44:48 +0800

    add

--- src/swap_nodes_in_pairs.c
+++ src/swap_nodes_in_pairs.c
@@ -14,6 +14,26 @@ struct ListNode {
     struct ListNode *next;
 };
 
+void *
+leet_malloc(size_t size)
+{
+    void *res = NULL;
+    if (size > 0) {
+        res = malloc(size);
+        memset(res,0,size);
+    }
+    return res;
+}
+
+struct ListNode *
+init_node(int val)
+{
+    struct ListNode *res;
+    res = leet_malloc(sizeof(struct ListNode));
+    res->val = val;
+    return res;
+}
+
 struct ListNode*
 swapPairs(struct ListNode* head)
 {
@@ -23,6 +43,17 @@ swapPairs(struct ListNode* head)
 int
 main(int argc, char **argv)
 {
+    struct ListNode *res_head, *res, *tmp, *result;
+    res_head = res = tmp = init_node(1);
+    tmp = init_node(2); res->next = tmp; res = tmp;
+    tmp = init_node(3); res->next = tmp; res = tmp;
+    tmp = init_node(4); res->next = tmp; res = tmp;
+    result = swapPairs(res_head);
+    printf("result print:\n");
+    while (result != NULL) {
+        printf("->%d",result->val);
+        result = result->next;
+    }
+    printf("\n");
     return 0; 
 }
-
