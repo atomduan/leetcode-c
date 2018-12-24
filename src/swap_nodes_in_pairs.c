aa382c5e90c931895b3aef2f46cea7ac76166a82
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2018-12-24 10:31:41 +0800

    add

---
+++ src/swap_nodes_in_pairs.c
@@ -0,0 +1,28 @@
+/*
+ *Given a linked list, swap every two adjacent nodes and return its head.
+ *Example:
+ *Given 1->2->3->4, you should return the list as 2->1->4->3.
+ *Note:
+ *Your algorithm should use only constant extra space.
+ *You may not modify the values in the list's nodes, only nodes itself may be changed.
+ */
+
+#include <linux_config.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode*
+swapPairs(struct ListNode* head)
+{
+    return NULL; 
+}
+
+int
+main(int argc, char **argv)
+{
+    return 0; 
+}
+
