260aeaf05facdb30f4f3b3010d981dd8b20cc647
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2018-12-24 11:25:00 +0800

    add

--- src/reverse_nodes_in_kgroup.c
+++ src/reverse_nodes_in_kgroup.c
@@ -43,11 +43,30 @@ init_node(int val)
 struct ListNode * 
 reverseKGroup(struct ListNode* head, int k)
 {
-    return NULL;    
+    return head;
 }
 
 int
 main(int argc, char **argv)
 {
+    int k=3;
+    struct ListNode *res_head, *res, *tmp, *result;
+    res_head = res = tmp = init_node(1);
+    tmp = init_node(2); res->next = tmp; res = tmp;
+    tmp = init_node(3); res->next = tmp; res = tmp;
+    tmp = init_node(4); res->next = tmp; res = tmp;
+    tmp = init_node(5); res->next = tmp; res = tmp;
+    tmp = init_node(6); res->next = tmp; res = tmp;
+    tmp = init_node(7); res->next = tmp; res = tmp;
+    tmp = init_node(8); res->next = tmp; res = tmp;
+    tmp = init_node(9); res->next = tmp; res = tmp;
+    tmp = init_node(10); res->next = tmp; res = tmp;
+    result = reverseKGroup(res_head,k);
+    printf("result print:\n");
+    while (result != NULL) {
+        printf("->%d",result->val);
+        result = result->next;
+    }
+    printf("\n");
     return 0; 
 }
