39dc39d07a5a685ce6931235a9d74534b2441f6c
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2019-01-08 19:33:25 +0800

    add

---
+++ src/longest_valid_parentheses.c
@@ -0,0 +1,30 @@
+/**
+ * Given a string containing just the characters '(' and ')', 
+ * find the length of the longest valid (well-formed) parentheses substring.
+ * 
+ * Example 1:
+ * Input: "(()"
+ * Output: 2
+ * Explanation: The longest valid parentheses substring is "()"
+ * 
+ * Example 2:
+ * Input: ")()())"
+ * Output: 4
+ * Explanation: The longest valid parentheses substring is "()()"
+ *
+ */
+#include <linux_config.h>
+
+int 
+longestValidParentheses(char* s)
+{
+    return 0;
+}
+
+int
+main(int argc,char **argv)
+{
+    char *s = "(()";
+    printf("result is %d\n",longestValidParentheses(s));
+    return 0;
+}
