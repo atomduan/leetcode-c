e9ca6b6db8c4b5c3a32fd97fd17e611bb3985546
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2019-01-11 17:14:15 +0800

    add

--- src/longest_valid_parentheses.c
+++ src/longest_valid_parentheses.c
@@ -15,16 +15,51 @@
  */
 #include <linux_config.h>
 
+int
+parenthese_scan(char *s)
+{
+    int count = 0, index = 0, res = 0, last = 0;
+    char *ts = s;
+    while (*ts != '\0') {
+        if (*ts == '(') {
+            count++;
+        } else
+        if (*ts == ')') {
+            count--;
+        }
+        if (count < 0)  {
+            index++;
+            goto result_fin;
+        }
+        if (count == 0) {
+            res += (index - last + 1);
+            last = index+1;
+        }
+        ts++; index++;
+    }
+result_fin:
+    return res;
+}
+
 int 
 longestValidParentheses(char* s)
 {
-    return 0;
+    int max = 0, size = 0;
+    char *ts = s;
+    while (*ts != '\0') {
+        size = parenthese_scan(ts); 
+        max = size > max ? size : max;
+        ts += size > 0 ? size : 1;
+    }
+    return max;
 }
 
 int
 main(int argc,char **argv)
 {
-    char *s = "(()";
+    char *s = ")()())";
+    //char *s = "(()";
+    //char *s = "()((())()";
     printf("result is %d\n",longestValidParentheses(s));
     return 0;
 }
