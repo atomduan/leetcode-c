af8a9a82801c8492366676d56a8747c0cc9d76d6
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2019-01-15 20:32:37 +0800

    add

---
+++ src/combination_sum.c
@@ -0,0 +1,45 @@
+/**
+ * Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), 
+ * find all unique combinations in candidates where the candidate numbers sums to target.
+ * 
+ * The same repeated number may be chosen from candidates unlimited number of times.
+ * Note:
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
+ * 
+ * Example 1:
+ * Input: candidates = [2,3,6,7], target = 7,
+ * A solution set is:
+ * [
+ *   [7],
+ *   [2,2,3]
+ * ]
+ * 
+ * Example 2:
+ * Input: candidates = [2,3,5], target = 8,
+ * A solution set is:
+ * [
+ *   [2,2,2,2],
+ *   [2,3,3],
+ *   [3,5]
+ * ]
+ */
+#include <linux_config.h>
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *columnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int ** 
+combinationSum(int* candidates, int candidatesSize, int target, 
+               int** columnSizes, int* returnSize) 
+{
+    return NULL; 
+}
+
+int
+main(int argc, char **argv)
+{
+    return 0;
+}
