c79906cbc06028190201e654e049a7b050e27c51
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2019-02-28 17:07:43 +0800

    add

---
+++ src/trapping_rain_water.c
@@ -0,0 +1,25 @@
+/**
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+ * 
+ * Example:
+ * Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+ * Output: 6
+ */
+
+#include <linux_config.h>
+
+int trap(int* height, int heightSize) {
+    return 63;        
+}
+
+/**
+ * TODO need implement
+ */
+int
+main(int argc, char ** argv)
+{
+    int height[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+    int heightSize = 12;
+    printf("return result is %d\n", trap(height, heightSize));
+    return 0;
+}
