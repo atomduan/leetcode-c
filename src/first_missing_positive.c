e0ebc7c7cba8cc749b3ba7e562a144a17c1bc5b1
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2019-02-28 16:57:33 +0800

    add

--- src/first_missing_positive.c
+++ src/first_missing_positive.c
@@ -18,11 +18,93 @@
  */
 #include <linux_config.h>
 
-void *
+#define LEET_SET_SLOT_SIZE 73
+
+typedef struct leet_hash_node_s leet_hash_node;
+struct leet_hash_node_s {
+    int val;
+    leet_hash_node *next;
+};
+
+typedef struct leet_hash_set_s leet_hash_set;
+struct leet_hash_set_s {
+    leet_hash_node **slot; 
+    int slot_size;
+};
+
+static void *
 leet_malloc(size_t size)
 {
     void *res = malloc(size);
     memset(res,0,size);
+    return res;
+}
+
+static leet_hash_node *
+leet_hash_node_new(int val)
+{
+    leet_hash_node *res = leet_malloc(sizeof(leet_hash_node));    
+    res->val = val;
+    return res;
+}
+
+static int
+leet_hash_code(leet_hash_set *set, int key)
+{
+    int slot_size = set->slot_size;
+    key = key<0 ? -key:key;
+    return key % slot_size;
+}
+
+static leet_hash_set *
+leet_hash_set_new()
+{
+    leet_hash_set *res = leet_malloc(sizeof(leet_hash_set));    
+    res->slot_size = LEET_SET_SLOT_SIZE;
+    leet_hash_node **slot = leet_malloc(LEET_SET_SLOT_SIZE*sizeof(leet_hash_node *));
+    res->slot = slot;
+    return res;
+}
+
+static int
+leet_hash_set_put(leet_hash_set *set, int key)
+{
+    int hc = leet_hash_code(set,key);
+    leet_hash_node **slot = set->slot;
+    leet_hash_node *entry = slot[hc];
+    if (entry == NULL) {
+        slot[hc] = leet_hash_node_new(key);
+        return 1;
+    } else {
+        while (entry != NULL) {
+            if (entry->val == key)  return 0;  
+            if (entry->next != NULL) {
+                entry = entry->next;
+                continue;
+            } else {
+                entry->next = leet_hash_node_new(key);
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+static int*
+leet_dedup_nums(int* nums, int *numsSize)
+{
+    int nsz = *numsSize;
+    int *res = leet_malloc((nsz+1)*sizeof(int));
+    int i=0,j=0;
+    leet_hash_set *dict = leet_hash_set_new();
+    for (i=0,j=0; i<nsz; i++) {
+        if (nums[i] <= 0) continue;
+        if (leet_hash_set_put(dict,nums[i])) {
+            res[j++] = nums[i];
+        }
+    }
+    *numsSize = j;
+    free(dict);
     return res;
 }
 
@@ -33,19 +115,24 @@ firstMissingPositive(int* nums, int numsSize)
     max = 0;
     min = INT_MAX;
     posn = 0;
+
+    //dedup nums
+    nums = leet_dedup_nums(nums,&numsSize);
+
+    //statistic
     for (i=0; i<numsSize; i++) {
         if (nums[i]>0 && nums[i]>max) max = nums[i];
         if (nums[i]>0 && nums[i]<min) min = nums[i];
         if (nums[i]>0) posn++;
     }
-    if (min > 1) return 1;
+    if (min >  1) return 1;
+    if (max <= 0) return 1;
     if (min+posn-1 < max) {
         dict = leet_malloc((posn+1)*sizeof(int));
         for (i=0; i<numsSize; i++) {
             if (nums[i]<=0) continue;
             if (min+posn < nums[i]) continue;
             idx = nums[i] - min; 
-            //printf("min:%d,max:%d,ival:%d,idx:%d,posn:%d\n",min,max,nums[i],idx,posn);
             dict[idx] = 1;
         }
         for (i=0;;i++)
@@ -61,7 +148,7 @@ main(int argc, char **argv)
     //int nums[] = {7,8,9,11,12};
     //int nums[] = {3,4,-1,1};
     //int nums[] = {0,1,2};
-    int nums[] = {1,3};
+    int nums[] = {1,3,3,76,76};
     int numsSize = sizeof(nums)/sizeof(int);
     int result = firstMissingPositive(nums,numsSize);
     printf("result is :%d\n", result);
---
+++ src/hash_set.c
@@ -0,0 +1,84 @@
+/**
+ * Quick sort DEMO
+ */
+#include <linux_config.h>
+
+#define LEET_SET_SLOT_SIZE 73
+
+typedef struct leet_hash_node_s leet_hash_node;
+struct leet_hash_node_s {
+    int val;
+    leet_hash_node *next;
+};
+
+typedef struct leet_hash_set_s leet_hash_set;
+struct leet_hash_set_s {
+    leet_hash_node **slot; 
+    int slot_size;
+};
+
+static void *
+leet_malloc(size_t size)
+{
+    void *res = malloc(size);
+    memset(res,0,size);
+    return res;
+}
+
+static leet_hash_node *
+leet_hash_node_new(int val)
+{
+    leet_hash_node *res = leet_malloc(sizeof(leet_hash_node));    
+    res->val = val;
+    return res;
+}
+
+static int
+leet_hash_code(leet_hash_set *set, int key)
+{
+    int slot_size = set->slot_size;
+    key = key<0 ? -key:key;
+    return key % slot_size;
+}
+
+leet_hash_set *
+leet_hash_set_new()
+{
+    leet_hash_set *res = leet_malloc(sizeof(leet_hash_set));    
+    res->slot_size = LEET_SET_SLOT_SIZE;
+    leet_hash_node **slot = leet_malloc(LEET_SET_SLOT_SIZE*sizeof(leet_hash_node *));
+    res->slot = slot;
+    return res;
+}
+
+int
+leet_hash_set_put(leet_hash_set *set, int key)
+{
+    leet_hash_node *curr_node;
+    int hc = leet_hash_code(set,key);
+    leet_hash_node **slot = set->slot;
+    leet_hash_node *entry = slot[hc];
+    if (entry == NULL) {
+        curr_node = leet_hash_node_new(key);
+        entry = curr_node;
+        return 1;
+    } else {
+        while (entry != NULL) {
+            if (entry->val == key)  return 0;  
+            if (entry->next != NULL) {
+                entry = entry->next;
+                continue;
+            } else {
+                entry->next = leet_hash_node_new(key);
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int
+main(int argc,char **argv)
+{
+    return 0;
+}
