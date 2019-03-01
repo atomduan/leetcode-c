763ad603f0aaca22f6d6a1485e79bea1bdf0ac85
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2019-03-01 17:48:48 +0800

    add

--- src/multiply_strings.c
+++ src/multiply_strings.c
@@ -19,10 +19,52 @@
 
 #include <linux_config.h>
 
+#define leet_ctoi(c)  c-48
+#define leet_itoc(i)  i+48
+
+inline void *
+leet_malloc(size_t size)
+{
+    void *res = malloc(size);
+    memset(res,0,size);
+    return res;
+}
+
+char*
+multiply_deci(char *num, char *sig)
+{
+    char *res, *tmp, *np = num;
+    int sigv, crgv, mulr, num_size, i=0,j=0;
+    int dsts=0, ssts=0;
+    
+    for (num_size=0; *np!='\0'; np++, num_size++); np--;
+    res = leet_malloc(num_size+1);
+    tmp = leet_malloc(num_size+1);
+
+    sigv = leet_ctoi(*sig);
+    do {
+        crgv = leet_ctoi(*np--); 
+        mulr = crgv*sigv + dsts;
+        dsts = mulr/10;  
+        ssts = mulr%10;
+        tmp[i++] = leet_itoc(ssts);
+    } while(i<num_size);
+
+    if (dsts > 0)
+        tmp[i] = leet_itoc(dsts);
+
+    for (i=num_size,j=0; i>=0; i--) {
+        if (tmp[i]=='\0') continue;
+        res[j++] = tmp[i];
+    }
+    free(tmp);
+    return res;
+}
+
 char*
 multiply(char* num1, char* num2)
 {
-    return NULL; 
+    return multiply_deci("12123412342342142341234123423143241234233", "8"); 
 }
 
 /** 
@@ -31,5 +73,8 @@ multiply(char* num1, char* num2)
 int
 main(int argc,char **argv)
 {
+    char *num1 = "123";
+    char *num2 = "456";
+    printf("multiply result is %s\n", multiply(num1, num2)); 
     return 0;
 }
