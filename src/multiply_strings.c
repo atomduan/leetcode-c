1ba41d163cf1bdb76831d5fefb8afdf00d4b9eb6
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2019-03-05 19:05:39 +0800

    add

--- src/multiply_strings.c
+++ src/multiply_strings.c
@@ -32,6 +32,24 @@ leet_malloc(size_t size)
 }
 
 char*
+multiply_deci_shift(char *num, char *tmp, int shift_step)
+{
+    char *res, *np=num;
+    int num_size, i;
+    memset(tmp,0,TMP_SIZE);
+    for (num_size=0; *np!='\0'; np++, num_size++);
+    res = leet_malloc(num_size+1+shift_step);
+    for (i=0; i<num_size; i++) {
+        res[i] = num[i];
+    }
+    for (i=0; i<shift_step; i++) {
+        res[num_size+i] = '0';
+    }
+    return res;
+}
+
+
+char*
 multiply_deci_sigal(char *num, char *sig, char *tmp)
 {
     char *res, *np=num;
@@ -62,11 +80,65 @@ multiply_deci_sigal(char *num, char *sig, char *tmp)
 }
 
 char*
+sum_two_layer_nums(char *upper, char *lower, char *tmp)
+{
+    int u_sz=0, l_sz=0, i, j, ts, t=0, num_size=0;
+    int dsts=0, ssts=0;
+    char *unp=upper, *lnp=lower, *res; 
+    for (;*unp!='\0'; unp++) u_sz++;
+    for (;*lnp!='\0'; lnp++) l_sz++;
+
+    memset(tmp,0,TMP_SIZE);
+    i = u_sz - 1;
+    j = l_sz - 1;
+    while (1) {
+        if (i >=0 && j >=0) {
+            ts = leet_ctoi(upper[i--])+leet_ctoi(lower[j--])+dsts;
+        } else
+        if (i >=0 && j < 0) {
+            ts = leet_ctoi(upper[i--])+dsts;
+        } else
+        if (i <0 && j >= 0) {
+            ts = leet_ctoi(lower[j--])+dsts;
+        } else
+        if (i <0 && j < 0) {
+            if (dsts > 0) tmp[t++] = leet_itoc(dsts);
+            break;
+        }
+        dsts = ts/10;
+        ssts = ts%10;
+        tmp[t++] = leet_itoc(ssts);
+    }
+    
+    num_size = (u_sz>l_sz? u_sz:l_sz)+1;
+    res = leet_malloc(num_size);
+    for (i=num_size,j=0; i>=0; i--) {
+        if (tmp[i]=='\0') continue;
+        res[j++] = tmp[i];
+    }
+    return res;
+}
+
+char*
 multiply(char* num1, char* num2)
 {
     char *tmp = leet_malloc(TMP_SIZE);
-    return multiply_deci_sigal("444444444444444444444444444444444444444444444444", "0", tmp); 
+    char *res=NULL, *rtp, *rtp_tmp, *res_tmp, *n2p;
+    int num2_sz=0, i, shift_step=0;
+    for (n2p=num2; *n2p!='\0'; n2p++) num2_sz++;
+    for (i=num2_sz-1; i>=0; i--, shift_step++) {
+        rtp = multiply_deci_sigal(num1,&num2[i],tmp);
+        if (res == NULL) {
+            res = rtp;
+        } else {
+            rtp_tmp = multiply_deci_shift(rtp,tmp,shift_step);
+            res_tmp = sum_two_layer_nums(res,rtp_tmp,tmp);
+            free(rtp); free(res); free(rtp_tmp);
+            res = res_tmp;
+        }
+    }
     free(tmp);
+    return res;
 }
 
 /** 
@@ -75,7 +147,7 @@ multiply(char* num1, char* num2)
 int
 main(int argc,char **argv)
 {
-    char *num1 = "123";
+    char *num1 = "12312";
     char *num2 = "456";
     printf("multiply result is %s\n", multiply(num1, num2)); 
     return 0;
