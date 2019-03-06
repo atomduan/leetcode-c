039da56b76c10e33eb09f07c0021b87dcd963d32
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2019-03-06 19:56:31 +0800

    add

--- src/kth_smallest_element_in_a_bst.c
+++ src/kth_smallest_element_in_a_bst.c
@@ -9,10 +9,9 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
-typedef struct nlist_s nlist;
-struct nlist_s {
+struct nlist {
     int val;
-    nlist next;
+    struct nlist *next;
 };
 
 void *
@@ -24,47 +23,54 @@ leet_malloc(size_t size)
 }
 
 void
-append_list(nlist **head, int val)
+append_sort_list(struct nlist **head_pt, int val, int k)
 {
-    int count;
-    nlist *pre,*tmp,*nw;
-    if (*head == NULL) {
-        *head = leet_malloc(sizeof(nlist));
-        tmp = *head;
+    int count, add_flag, kbar;
+    kbar = k+1;
+    struct nlist *pre,*tmp,*nw;
+    if (*head_pt == NULL) {
+        *head_pt = leet_malloc(sizeof(struct nlist));
+        tmp = *head_pt;
         tmp->val = val;
     } else {
-        tmp = *head; pre = NULL;
-        for (tmp=head,count=0; tmp->next!=NULL; tmp++,count++) {
-            if (count < 5) {
+        tmp = *head_pt; pre = NULL; add_flag=0;
+        for (count=0; tmp!=NULL; count++) {
+            if (count < kbar) {
                 if (val < tmp->val) {
-                    nw = leet_malloc(sizeof(nlist));
+                    nw = leet_malloc(sizeof(struct nlist));
                     nw->val = val;
-                    nw->next = tmp;
                     if (pre == NULL) {
-                        *head = nw;
+                        *head_pt = nw;
                     } else {
                         pre->next = nw;
                     }
+                    nw->next = tmp;
+                    add_flag = 1;
                 }
-            } else {
+            }
+            //clean tail, if tmp is the last
+            if (tmp->next==NULL && count>=kbar && add_flag==1) {
                 pre->next = NULL;
                 free(tmp);
+                break;
+            } else {
+                pre = tmp;
+                tmp = tmp->next;
             }
-            pre = tmp;
         }
     }
 }
 
 void
-doKthSmallest(struct TreeNode* root, int k, nlist **head)
+doKthSmallest(struct TreeNode* root, int k, struct nlist **head_pt)
 {
     int val = root->val;
-    append_list(head,val);
+    append_sort_list(head_pt,val,k);
     if (root->left != NULL) {
-        doKthSmallest(root->left,head);
+        doKthSmallest(root->left,k,head_pt);
     }
     if (root->right != NULL) {
-        doKthSmallest(root->right,head);
+        doKthSmallest(root->right,k,head_pt);
     }
 }
 
@@ -72,9 +78,9 @@ int
 kthSmallest(struct TreeNode* root, int k)
 {
     int i;
-    nlist *head = NULL;
+    struct nlist *head = NULL;
     doKthSmallest(root,k,&head);
-    for (int i=0; i<4; i++) {
+    for (i=0; i<k-1; i++) {
         head++;
     }
     return head->val;
