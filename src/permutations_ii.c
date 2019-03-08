9c1ea779ceaca31fb7bcd8658af2a8aa3d358e09
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2019-03-08 15:08:59 +0800

    add

--- src/permutations_ii.c
+++ src/permutations_ii.c
@@ -11,7 +11,55 @@
  *   [2,1,1]
  * ]
  */
+
 #include <linux_config.h>
+
+typedef leet_queue_s leet_queue;
+struct leet_queue_s {
+    int *nums; 
+    leet_queue *next;
+};
+
+void *
+leet_malloc(size_t size)
+{
+    void *res = malloc(size);
+    memset(res,0,size);
+    return res;
+}
+
+void
+leet_queue_append(leet_queue **head, int *nums)
+{
+    leet_queue *res;
+    leet_queue *node = leet_malloc(sizeof(leet_queue));
+    node->nums = nums;
+    if (*head == NULL) {
+        *head = node;
+    } else {
+        res = *head;
+        while (1) {
+            if (res == NULL) break;
+            if (res->next == NULL) {
+                res->next = node;
+                break;
+            } else {
+                res = res->next;
+                continue;
+            }
+        }
+    }
+}
+
+int
+leet_head_size(int numsSize)
+{
+    int res = 1, i;
+    for (i=0; i<numsSize; i++) {
+        res *= i+1; 
+    }
+    return res;
+}
 
 /**
  * Return an array of arrays of size *returnSize.
@@ -20,6 +68,8 @@
 int**
 permuteUnique(int* nums, int numsSize, int* returnSize)
 {
+    int head_size = (leet_head_size(numsSize)+1)*sizeof(int*);
+    int **head == leet_malloc(head_size);
     return NULL; 
 }
 
